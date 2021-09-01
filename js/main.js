9101b9a4cc35d7aba1bcf44b7085cc116291588d
Sayaka <j.sadolf1123@gmail.com>
2021-09-01 13:40:19 +0900

    update

--- js/main.js
+++ js/main.js
@@ -10,28 +10,49 @@ $(function(){
 function input_check() {
   var result = true;
 }
-	// エラー用装飾のためのクラスリセット
-	$('#company-name').removeClass("inp_error");
-	$('#name').removeClass("inp_error");
-	$('#e-mail').removeClass("inp_error");
-	$('#message').removeClass("inp_error");
-	// 入力エラー文をリセット
-	$("#company-name").empty();
-	$("#name").empty();
-	$("#e-mail").empty();
-	$("#message").empty();
-	// 入力内容セット
-	var name   = $("#company-name").val();
-	var furigana  = $("#name").val();
-	var username = $("#e-mail").val();
-	var mailaddress  = $("#message").val();
-	// 入力内容チェック
-	// お問い合わせ内容
-	if(remarks == ""){
-		$("#remarks_error").html("!この フィールドを入力して下さい。");
-		$("#remarks").addClass("inp_error");
+// エラー用装飾のためのクラスリセット
+	$('#company_name').removeClass("inp_error");
+	$('#username').removeClass("inp_error");
+	$('#e_mail').removeClass("inp_error");
+	$('#comment').removeClass("inp_error");
+// 入力エラー文をリセット
+	$("#company_name_error").empty();
+	$("#username_error").empty();
+	$("#mailaddress_error").empty();
+	$("#comment_error").empty();
+// 入力内容セット
+	var company_name   = $("#company_name").val();
+	var username  = $("#username").val();
+	var e_mail = $("#e_mail").val();
+	var comment = $("#comment").val();
+// 入力内容チェック
+	//会社名
+if (company_name == "") {
+		$("#company_name_error").html("! このフィールドを入力して下さい。");
+		$("#company_name").addClass("inp_error");
 		result = false;
-	return result;
+return result;
+}
+	//名前
+if (username == "") {
+		$("#username_error").html("! このフィールドを入力して下さい。");
+		$("#username").addClass("inp_error");
+		result = false;
+return result;
+}
+	//メールアドレス
+if (e_mail == "") {
+		$("#mailaddress_error").html("! このフィールドを入力して下さい。");
+		$("#e_mail").addClass("inp_error");
+		result = false;
+return result;
+}
+	// お問い合わせ内容
+if (comment == "") {
+		$("#comment_error").html("! このフィールドを入力して下さい。");
+		$("#comment").addClass("inp_error");
+		result = false;
+return result;
 }
 // レスポンシブデザイン:ハンバーガーメニュー,ドロワーメニュー
 const target = document.getElementById("menu");
